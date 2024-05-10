3e9f58926d4151d963e90bc44ff81992d6527549
Grinko1 <nadenka64013730@gmail.com>
2024-05-10 12:25:34 +0900

    add volume for db, and nodemon for restart

--- api/src/index.js
+++ api/src/index.js
@@ -1,22 +1,48 @@
 const express = require('express');
 const { connectDb } = require('./helper/db');
 const { port, host, db } = require('./config');
+const mongoose = require('mongoose');
 const app = express()
 
+const postSchema = new mongoose.Schema({
+    name: String
+})
+const Post = mongoose.model('Post', postSchema)
 
+const createPosts = async () => {
+    const onePost = new Post({ name: "First post" })
+    const twoPost = new Post({ name: "Two post" })
+    const threePost = new Post({ name: "Three post" })
+    await onePost.save()
+    await twoPost.save()
+    await threePost.save()
+
+}
 
 const startServer = () => {
-    app.listen(port, () => {
+    app.listen(port, async () => {
         console.log(`Started api service on port ${port}`);
         console.log(`Host from env is ${host}`);
         console.log(`Db url is ${db}`);
+        // createPosts()
+
+        const posts = await Post.find()
+        posts.forEach(p => {
+            console.log("posts in db", p.name);
+        })
+
+
+        // const newPost = new Post({ name: "First post" })
+
+        // const newPost = new Post({ name: "First post" })
+        // console.log(newPost);
     })
 }
 
 
 
 app.get('/test', (req, res) => {
-    res.send('Our api server is working correctly')
+    res.send('Our api server is working correctly with auto updates')
 })
 
 connectDb()
